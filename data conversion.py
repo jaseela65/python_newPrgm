48391a92209cbfb3d2bf2772c406178bec08202f
jaseela65 <62145927+jaseela65@users.noreply.github.com>
2020-03-13 23:39:47 +0530

    finish
    data type conversion

---
+++ data conversion.py
@@ -0,0 +1,110 @@
+
+
+a=5
+b=2.5
+c="Hello"
+d=True
+
+print("conversion of integer to float")
+e= float(a)
+print(type(a))
+print(type(e))
+print(e)
+
+
+print("conversion of integer to string")
+f=str(a)
+print(type(a))
+print(type(f))
+print(f)
+
+
+print("conversion of integer to Boolean")
+g=bool(a)
+print(type(a))
+print(type(g))
+print(g)
+
+print("conversion of float to integer")
+h=int(b)
+print(type(b))
+print(type(h))
+print(h)
+
+print("conversion of float to string")
+i=str(b)
+print(type(b))
+print(type(i))
+print(i)
+
+print("conversion of float to boolean")
+j=bool(b)
+print(type(b))
+print(type(j))
+print(j)
+
+'''
+print("conversion of string to integer")
+k=int(c)
+print(type(c))
+print(type(k))
+print(k)
+
+print("conversion of string to float")
+l=float(c)
+print(type(c))
+print(type(l))
+print(l)
+
+'''
+print("conversion of string to boolean")
+m=bool(c)
+print(type(c))
+print(type(m))
+print(m)
+
+
+print("conversion of boolean to integer")
+n=int(d)
+print(type(d))
+print(type(n))
+print(n)
+
+print("conversion of boolean to Float")
+o=float(d)
+print(type(d))
+print(type(o))
+print(o)
+
+print("conversion of boolean to string")
+p=str(d)
+print(type(d))
+print(type(p))
+print(p)
+
+q='2'
+print("conversion of number string to integer")
+r=int(q)
+print(type(q))
+print(type(r))
+print(r)
+
+print("conversion of number string to float")
+s=float(q)
+print(type(q))
+print(type(s))
+print(s)
+
+print("conversion of number string to bool")
+t=bool(q)
+print(type(q))
+print(type(t))
+print(t)
+
+
+
+
+
+
+
+
