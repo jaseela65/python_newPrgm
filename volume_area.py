8920cb30447c0fa458545c658b454d4e06143705
jaseela65 <62145927+jaseela65@users.noreply.github.com>
2020-03-14 00:35:24 +0530

    finish
    volume,area,circumference

---
+++ volume_area.py
@@ -0,0 +1,24 @@
+pi=22/7
+r=5
+h=8
+v=int(1/3*pi*r*r*h)
+print("Volume = ",v)
+
+
+n=10
+p=7*12 
+r=5/100
+s=float(n*p*r)
+print("npr=",s)
+
+
+r=5.8
+w=int(pi*r*r)
+print("area of circle = ",w)
+
+
+r=10.5
+x=int(2*pi*r)
+print("circumference  = ",x)
+
+
