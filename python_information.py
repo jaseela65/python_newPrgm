6dcda8e272e0302886910f4516eae230991dcea7
jaseela65 <62145927+jaseela65@users.noreply.github.com>
2020-03-19 16:48:51 +0530

    Add files via upload

---
+++ python_information.py
@@ -0,0 +1,128 @@
+  1 , Difference between python2 and python3 ?
+
+  python2
+
+  #1 To store unicode string value you required to define them with 'u'.
+
+  #2 value of global variable is change while using it inside for loop.
+
+  #3 Xrange() function is used for iteration.
+
+  #4 Function print is like - print "hello".
+
+  #5 in case of exceptions , it should be enclosed in notations.
+
+
+  python3
+
+  #1 Default storing of string is unicode.
+
+  #2 Value of variables never change.
+
+  #3  newRange() , function is introduced to perform iterations.
+
+  #4 Function print is like - print ("hello").
+
+  #5 in case of exceptions , it should be enclosed in parenthesis.
+
+
+  2,Difference between compiler and interpreter ?
+
+   Compiler
+
+  #1  Compiler works on the complete program at once and it takes the entire program as input.
+
+  #2  Compiler generates intermediate code,called the object code or machine code.
+
+  #3  Compiler executes conditional control statements and logical constructs faster than interpreter.
+
+  #4  compiled programs take more memory because the entire project code has to remain in memory.
+
+  #5  Errors are reported after the entire program is checked for syntatical and other errors.
+
+   Interpreter
+
+  #1 interpreter program work line by line . it takes one statement at a time as input.
+
+  #2 interpreter does not generate intermediate object code  or machinecode.
+
+  #3 interpreter execute a conditional statements at a much slower speed.
+
+  #4 interpreter does not generate intermediate object code .As a result interpreted programs are more memory efficient.
+
+  #5 Error is reported as soon as the first error is encountered.Rest of the program will not be checked untill the existing error is removed.
+
+
+ 3,Good Features of python programming language?
+
+ #1 Easy to code and High level language.
+
+ #2 Free and Open Source.
+
+ #3 Object Oriented language.
+
+ #4 GUI programming support.
+
+ #5 Python is portable language.
+ 
+
+ 4, What is escape sequence ? Explain with example ?
+
+ #1 Escape sequence is a sequence of characters that does not represent itself.
+
+ #2 it is used inside a character or string literal.
+
+ #3 it is composed of two or more characters starting with backslash.
+
+  example: \n  represents new line.
+
+           \b  represents back space.
+
+           \t  represents  tab(horizontal)
+
+           \v represents vertical
+           
+
+
+  
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
