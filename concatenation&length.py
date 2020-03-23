660a28ec7743d19fcec18d69412eae8dabbfafd2
jaseela65 <62145927+jaseela65@users.noreply.github.com>
2020-03-23 09:27:33 +0530

    Add files via upload

---
+++ concatenation&length.py
@@ -0,0 +1,11 @@
+a=input()
+b=input()
+c=a+b
+print(c)
+print(len(c))
+d=a[0:6:5]
+e=b[0:4:3]
+print(d+e)
+
+
+
---
+++ printSpecificString.py
@@ -0,0 +1,5 @@
+a=input()
+f=a[0:5:5].upper()
+g=a[1:5]
+h=a[-1].upper()
+print(f+g+h)
