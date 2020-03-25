9c0899f982e19593dc8d6578efb01f98dcad319d
jaseela65 <62145927+jaseela65@users.noreply.github.com>
2020-03-25 19:05:30 +0530

    Add files via upload

---
+++ different_string_formats.py
@@ -0,0 +1,34 @@
+  # Task1
+
+a=input()
+b=a
+c=a.upper()
+print(b[0]+c[1]+b[2:4]+c[-2]+b[-1])  #output : pYthOn
+
+
+ # Task2
+a=input()
+b=input()
+print(a*7+b*6)  #output : pythonpythonpythonpythonpythonpythonpython777777
+
+
+# Task3
+a=input()
+b=a
+c=a.upper()
+d=c[int(len(c)/2)]
+print(c[0]+b[1:3]+d+b[-2]+c[-1]) #output : PytHoN
+
+
+ # Task4
+a=input()
+b=input()
+c=a+b
+d=c.upper()
+print(d[0]+c[1:5]+d[5:7]+c[7:9]+d[-1]) #output : PythoNJavA
+
+
+ # Task4
+a=input()
+b=input()
+print(a*3+b*5)  #output : 55533333
