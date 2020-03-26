b799404257817aa98d821316646f2cac1205131b
jaseela65 <62145927+jaseela65@users.noreply.github.com>
2020-03-26 19:49:52 +0530

    Add files via upload

---
+++ negative&positive_String_Indexing&slicing.py
@@ -0,0 +1,53 @@
+a="engineering_mathematics"
+
+print(a[-10:-3:1]) # Ans: athemat
+
+print(a[-5:5:5])   # Ans:
+
+print(a[-10:3:3])  # Ans:
+
+print(a[5:15:10])  # Ans: e
+
+print(a[0:-3:3])   # Ans: eienmha
+
+print(a[3:10:3])   # Ans: ien
+
+print(a[4:10:5])   # Ans: nn
+
+print(a[:-10:2])   # Ans: egneigm
+
+print(a[0:-9:9])   # Ans: en
+
+print(a[:3:3])     # Ans: e
+
+print(a[4::4])     # Ans: nimei
+
+print(a[0::5])     # Ans: eeghi
+
+print(a[10:-1:3])  # Ans: gaet
+
+print(a[-1::1])    # Ans: s
+
+print(a[0:0:1])    # Ans:
+
+print(a[-4:-1:10]) # Ans: t
+
+print(a[0:10:3])    # Ans:eien
+
+print(a[-15:-5:5])  # Ans: ia
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
