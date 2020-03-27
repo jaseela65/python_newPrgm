4405edd18264d5f00de883b3854a9da468b697cc
jaseela65 <62145927+jaseela65@users.noreply.github.com>
2020-03-27 17:00:48 +0530

    Add files via upload

---
+++ computer_science.py
@@ -0,0 +1,25 @@
+a="computer_science"
+print(a[2:-2:2])  #  mue_ce
+print(a[2:-2:-2])  #  no answer
+print(a[-2:2:2])   # no answer
+print(a[-2:2:-2]) #  cec_eu
+print(".....................")
+print(a[:5:-1])  # ecneics_re
+print(a[:5:1])   # compu
+print(a[:-5:1])   # computer_sc
+print(a[:-5:-1])  # ecne
+print(".....................")
+print(a[10::1])  # cience
+print(a[10::-1]) # cs_retupmoc
+print(a[10::-2]) # c_eumc
+print(a[10::-5])  # ctc
+print(a[-10:10:10]) # e
+print(".....................")
+print(a[::10])  #  cc
+print(a[::-5])  # ectc
+print(a[::-8])  # er
+print(a[:-8:2])  # cmue
+print(a[-5:5:2]) #  no answer
+print(".....................")
+print(a[-10:-1:-1]) # no answer
+print(a[-9:9:-2])   # no answer
