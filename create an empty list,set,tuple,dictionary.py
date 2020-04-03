401021e39bcdd5a8e5b0fd62d7d777dad43d7756
jaseela65 <62145927+jaseela65@users.noreply.github.com>
2020-04-03 20:34:20 +0530

    Add files via upload

---
+++ create an empty list,set,tuple,dictionary.py
@@ -0,0 +1,102 @@
+
+#  Python program to make an empty list
+
+a=[]
+print("values of a:",a)
+print(" \Type of a :",type(a))
+print("Length of a :",len(a))
+
+#output
+values of a: []
+Type of a : <class 'list'>
+Length of a : 0
+
+..........................................................
+
+#  Python program to make an empty tuple
+
+b=()
+print("values of b:",b)
+print("Type of b :",type(b))
+print("Length of b :",len(b))
+
+#output
+
+values of b: ()
+Type of b : <class 'tuple'>
+Length of b : 0
+
+...........................................................
+
+#  Python program to make an empty dictionary
+
+c={}
+print("values of c:",c)
+print("Type of c :",type(c))
+print("Length of c :",len(c))
+
+#output
+
+values of c: {}
+Type of c : <class 'dict'>
+Length of c : 0
+
+...........................................................
+
+#  Python program to make an empty set
+
+d=set()
+print("values of d:",d)
+print("Type of d :",type(d))
+print("Length of d :",len(d))
+
+#output
+
+values of d: set()
+Type of d : <class 'set'>
+Length of d : 0
+
+.............................................................
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
