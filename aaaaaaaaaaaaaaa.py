64f3f5c39e6817f849c51de1106a1d2ddc6a5e43
jaseela65 <62145927+jaseela65@users.noreply.github.com>
2020-05-02 20:35:00 +0530

    Add files via upload

---
+++ aaaaaaaaaaaaaaa.py
@@ -0,0 +1,4 @@
+main = {"question1": {"question":"what is your name ?","options":["a","b","c","d"],
+                      "correct answer":"c","explanation":"good name"},
+        "question2": {"question":"what is your agw ?","options":["a","b","c","d"],"correct answer":"c","explanation":"good age"}}
+#print(main)print(main["question1"]["question"])print(main["question1"]["options"])print(main["question2"]["question"])
